d05359a5a632ac27d89e6229939dad3b0795e498
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-07-18 11:37:06 +0530

    adds security group rule for postgres

--- vpc.tf
+++ vpc.tf
@@ -117,6 +117,15 @@ resource "aws_security_group" "vpc_private_sg" {
       "${var.vpc_public_subnet_1_cidr}"]
   }
 
+  # allow postgres port within VPC
+  ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    cidr_blocks = [
+      "${var.vpc_public_subnet_1_cidr}"]
+  }
+
   egress {
     from_port = "0"
     to_port = "0"
